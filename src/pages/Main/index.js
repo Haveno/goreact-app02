709de588967f9c8341d39107666cf89244532f32
Hugo Sukeyosi <hugosukeyosi@gmail.com>
2019-05-24 00:59:26 -0400

    app02

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.png';
 
@@ -6,17 +8,55 @@ import { Container, Form } from './styles';
 
 import CompareList from '../../components/CompareList';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
+export default class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+  };
 
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">Ok</button>
-    </Form>
+  handleAddRepository = async (e) => {
+    e.preventDefault();
 
-    <CompareList />
-  </Container>
-);
+    this.setState({ loading: true });
 
-export default Main;
+    try {
+      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, repository],
+        repositoryError: false,
+      });
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form withError={this.state.repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">
+            {this.state.loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}
+          </button>
+        </Form>
+
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
+}
